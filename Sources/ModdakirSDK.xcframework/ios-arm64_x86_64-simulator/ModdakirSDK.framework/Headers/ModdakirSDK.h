20e7dde8f3eebf5d85d207ae53b3de7b3e3b80cc
Mahmoud Moussa <m.moussa@moddakir.com>
2025-11-16 17:56:32 +0200

    fix the delay of ending call

---
+++ Sources/ModdakirSDK.xcframework/ios-arm64_x86_64-simulator/ModdakirSDK.framework/Headers/ModdakirSDK.h
@@ -0,0 +1,16 @@
+//
+//  ModdakirSDK.h
+//  ModdakirSDK
+//
+//  Created by Amin on 5/16/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ModdakirSDK.
+FOUNDATION_EXPORT double ModdakirSDKVersionNumber;
+
+//! Project version string for ModdakirSDK.
+FOUNDATION_EXPORT const unsigned char ModdakirSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModdakirSDK/PublicHeader.h>
